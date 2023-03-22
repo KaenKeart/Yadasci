41c263e13bf481f3160bfc3fb141eec6522f43e7
KaenKeart <128370953+KaenKeart@users.noreply.github.com>
2023-03-23 00:57:13 +0700

    Kaen

--- my-product-app/src/index.js
+++ my-product-app/src/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Router from './Router';
+import Project from './Project_01';
+import Page01 from './Page01';
+import Page02 from './Page02';
+import Page03 from './Page03';
+import Page04 from './Page04';
+//import Home from './Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <App />
-  </BrowserRouter>
-    
-  
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Project />} />
+      <Route path="/page01" element={<Page01 />} />
+      <Route path="/page02" element={<Page02 />} />
+      <Route path="/page03" element={<Page03 />} />
+      <Route path="/page04" element={<Page04 />} />
+
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
