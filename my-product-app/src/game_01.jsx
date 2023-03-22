6a8c88c3dff352a8064c6a97ab5814d57b32a66d
ณฐกร พิกรมสุข <nathakorn270617@gmail.com>
2023-03-23 06:03:16 +0700

    img

--- my-product-app/src/game_01.jsx
+++ my-product-app/src/game_01.jsx
@@ -1,5 +1,11 @@
+import { Carousel } from 'react-bootstrap';
 import Footer from './footer';
 import Navbar from './Navbar';
+
+import game_01 from '../src/img/game_01.jpg'
+import game_01_2 from '../src/img/game_01-2.jpg'
+
+
 
 function Game01() {
 
@@ -7,8 +13,17 @@ function Game01() {
 
 <div>
     <Navbar/>
-    <br/>
-    <h1>game</h1>   
+    <div id="slide_game">
+            <Carousel style={{width: "250px", height: "250px",float: 'left'}}>
+  <Carousel.Item interval={1000}>
+    <img src={game_01} className="img_responsive" alt="..." style={{height: "250px"}} />
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img src={game_01_2} className="img_responsive" alt="..." style={{height: "250px"}} />
+  </Carousel.Item>
+</Carousel>
+
+     </div>  
     
     <Footer/>
 </div>      
