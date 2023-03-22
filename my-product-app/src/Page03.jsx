18b903b5f2c10ceb774857d6599c18c89033abf4
ณฐกร พิกรมสุข <nathakorn270617@gmail.com>
2023-03-22 23:56:56 +0700

    img

--- my-product-app/src/Page03.jsx
+++ my-product-app/src/Page03.jsx
@@ -1,5 +1,10 @@
+import { Link } from 'react-router-dom';
 import Footer from './footer';
 import Navbar from './Navbar';
+import game_08 from '../src/img/game_08.jpg'
+import game_09 from '../src/img/game_09.jpg'
+import game_10 from '../src/img/game_12.jpg'
+import game_12 from '../src/img/game_12.jpg'
 
 function Page03() {
 
@@ -13,39 +18,39 @@ function Page03() {
   <main>
     <section>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_08.html"><img src="img/game_08.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_08.jsx"><img src={game_08} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>พลิกพิภพดาวอังคาร อาณานิคม</h6>
           <div className="text-end text-danger">950 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_09.html"><img src="img/game_09.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_09.jsx"><img src={game_09} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>พลิกพิภพดาวอังคาร ปฏิบัติการดาวศุกร์</h6>
           <div className="text-end text-danger">950 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_10.html"><img src="img/game_10.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_10.jsx"><img src={game_10} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>พลิกพิภพดาวอังคาร ปฐมบท</h6>
           <div className="text-end text-danger">650 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_12.html"><img src="img/game_12.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_12.jsx"><img src={game_12} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>คฤหาสน์วิปลาส ไขประตูสู่วิวรณ์</h6>
           <div className="text-end text-danger">1,100 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
     </section>
--- my-product-app/src/Page04.jsx
+++ my-product-app/src/Page04.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
 import Footer from './footer';
 import Navbar from './Navbar';
+import game_01 from '../src/img/game_01.jpg'
+import game_02 from '../src/img/game_02.jpg'
+import game_03 from '../src/img/game_03.jpg'
+import game_04 from '../src/img/game_04.jpg'
+import game_05 from '../src/img/game_05.jpg'
+import game_17 from '../src/img/game_17.jpg'
+import game_18 from '../src/img/game_18.jpg'
+import game_19 from '../src/img/game_19.jpg'
+import game_20 from '../src/img/game_20.jpg'
 
 function Page02() {
 
@@ -14,84 +24,84 @@ function Page02() {
   <main>
     <section>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_01.html"><img src="img/game_01.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_01.jsx"><img src={game_01} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>คฤหาสน์วิปลาส</h6>
           <div className="text-end text-danger">3,650 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_02.html"><img src="img/game_02.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_02.jsx"><img src={game_02} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>เกมโค่นอำนาจ</h6>
           <div className="text-end text-danger">700 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_03.html"><img src="img/game_03.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_03.jsx"><img src={game_03} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>7 สิ่งมหัศจรรย์</h6>
           <div className="text-end text-danger">1,800 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_04.html"><img src="img/game_04.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_04.jsx"><img src={game_04} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>เกมล่าปริศนามนุษย์หมาป่า</h6>
           <div className="text-end text-danger">850 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_05.html"><img src="img/game_05.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_05.jsx"><img src={game_05} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>ปีกปักษา</h6>
           <div className="text-end text-danger">1,900 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_17.html"><img src="img/game_17.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_17.jsx"><img src={game_17} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>เหมียว ระเบิด</h6>
           <div className="text-end text-danger">950 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_18.html"><img src="img/game_18.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_18.jsx"><img src={game_18} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>ผู้ตรวจการแห่งนอตติงแฮม</h6>
           <div className="text-end text-danger">1,450 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_19.html"><img src="img/game_19.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_19.jsx"><img src={game_19} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>พลิกพิภพดาวอังคาร</h6>
           <div className="text-end text-danger">2,450 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
       <div className="card float-start me-4" style={{width: '18rem'}}>
-        <a href="game_20.html"><img src="img/game_20.jpg" className="card-img-top" alt="..." width={250} height={250} /></a>
+        <Link to="game_20.jsx"><img src={game_20} className="card-img-top" alt="..." width={250} height={250} /></Link>
         <div className="card-body">
           <h6 className="card-title" style={{minHeight: 70}}>เกมค้าเพชร</h6>
           <div className="text-end text-danger">1,500 ราคา
           </div>
-          <a href="#" className="btn btn-primary">หยิบใส่ตะกร้า</a>
+          <Link href="#" className="btn btn-primary">หยิบใส่ตะกร้า</Link>
         </div>
       </div>
     </section>
