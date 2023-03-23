2d8455d9ca00db8f082aec0988f5b7ec2d192669
ญาดา สุวรรณโชติ <yadasu11@gmail.com>
2023-03-24 05:56:47 +0700

    al

--- my-product-app/src/Productitem.js
+++ my-product-app/src/Productitem.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import FavCart from "./FavCart";
 
 export default function ProductItem(props) {
 
@@ -17,13 +18,19 @@ export default function ProductItem(props) {
 
 
         <div className="card-body">
+            
           <h6 className="card-title" style={{minHeight: 70}}>{props.data.product_name}</h6>
+          {!props.showButtons && (
+                    <FavCart />
+                )
+          }
           {props.showButtons && (
                     <>
                         <Link to={`/product/${props.data.product_id}`} className="btn btn-outline-primary me-3">แก้ไข</Link>
                         <button type="button" className="btn btn-outline-danger" onClick={onDelete} style={{ width: "80px" }}>ลบ</button>
                     </>
-                )}
+                )
+                }
           <div className="text-end text-danger">{props.data.price} ราคา</div>
 
             
