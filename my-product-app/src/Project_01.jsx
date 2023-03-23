45adb23e7960186e835af3b5cbf847120815dd12
ญาดา สุวรรณโชติ <yadasu11@gmail.com>
2023-03-24 04:52:16 +0700

    connectphp

--- my-product-app/src/Project_01.jsx
+++ my-product-app/src/Project_01.jsx
@@ -10,9 +10,73 @@ import Game24 from '../src/img/game_24.jpg'
 import FavCart from './FavCart'
 import Footer from './footer'
 import Navbar from './Navbar'
-import style from './Project_01.css';
+import style from './Project_01.css'
+import { API_GET, API_POST } from "./api"
+import ProductItem from "./Productitem"
+import { useEffect, useState } from "react"
 
 function Project() {
+  const [productTypes, setProductTypes] = useState([]);
+    const [productTypeId, setProductTypeId] = useState(0);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                "http://localhost:8000/api/product_types",
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        'content-Type': 'application/json',
+                        Authorization: "Bearer " + localStorage.getItem("access_token")
+                    }
+                }
+            );
+
+            let json = await response.json();
+            setProductTypes(json.data);
+        }
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                "http://localhost:8000/api/products/type/" + productTypeId,
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        'content-Type': 'application/json',
+                        Authorization: "Bearer " + localStorage.getItem("access_token")
+                    }
+                }
+            );
+
+            const json = await response.json();
+            setProducts(json.data);
+        }
+
+        fetchData();
+    }, [productTypeId]);
+
+    const fetchProducts = async () => {
+        let json = await API_GET("products/type/" + productTypeId);
+        setProducts(json.data);
+    }
+
+    const onDelete = async (data) => {
+        let json = await API_POST("product/delete", {
+            product_id: data.product_id
+        });
+
+        if (json.result) {
+            fetchProducts();
+        }
+    }
+
 
     return (
         <div>
@@ -80,6 +144,18 @@ function Project() {
     <span style={{color: '#5B785B'}}>มาใหม่</span>
   </h1>
   <main>
+  <div className="container mt-3">
+    {
+        products.filter(item => [22, 23, 24].includes(item.product_id))
+        .map(item => (
+            <ProductItem 
+                key={item.product_id}
+                data={item} 
+                onDelete={onDelete} 
+            />
+        ))
+    }
+</div>
     
     <section>
       <div className="card float-start me-4" style={{width: '18rem'}}>
